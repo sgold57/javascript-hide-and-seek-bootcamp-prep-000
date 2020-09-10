d7cc6a53a5f39bf57b5a0518bf3835477357e4c9
carefree-request-1153 <sgold5757@gmail.com>
2020-09-10 21:11:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("div#nested").querySelector(".target")
+}
+
+function increaseRankBy(n) {
+  const ranks = document
+                .getElementById("app")
+                .querySelectorAll("ul.ranked-list li")
+
+      for (let i = 0; i < ranks.length; i++) {
+        ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n).toString()
+      }
+  return
+}
+
+function deepestChild() {
+  const grandNode = document
+                    .getElementById("app")
+                    .querySelector("div#grand-node")
+
+  const offspring = grandNode.children
+  return offspring
+}
